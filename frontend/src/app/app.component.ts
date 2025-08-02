5e56b0719a99289fd62632a1dea4c5bc7b77c8a8
dadames01 <96684103+dadames01@users.noreply.github.com>
2025-08-02 17:20:16 +0200

    feat: add candidates form (#1)
    * chore: delete sample code
    
    * feat: add initial version for candidates form

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormControl, ReactiveFormsModule, FormGroup } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule, MatFormFieldModule, MatInputModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'frontend';
+  candidatesForm = new FormGroup({
+    name: new FormControl(''),
+    surname: new FormControl(''),
+    otherData: new FormControl('')
+  })
+
+  onFileSelected(event: any) {
+    console.log(event)
+  }
+
+  onSubmit() {
+
+  }
 }
