57a5d6319301e3d4fe01f824617236ff17ed01b4
Daniel Adames <danieladames24@gmail.com>
2025-08-03 18:13:39 +0200

    feat: add candidate info processing

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  Body,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { AppService } from './app.service';
+import * as multer from 'multer';
 
-@Controller()
+@Controller('candidates')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post()
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: multer.memoryStorage(),
+    }),
+  )
+  processCandidate(
+    @UploadedFile() file: Express.Multer.File,
+    @Body('name') name: string,
+    @Body('surname') surname: string,
+  ) {
+    return this.appService.processCandidate(name, surname, file.buffer);
   }
 }
