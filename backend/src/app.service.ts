54ef707f52a81f699f4e9c6ecfd221eb91568f6a
dadames01 <96684103+dadames01@users.noreply.github.com>
2025-08-03 18:21:07 +0200

    feat: create candidates api (#2)

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import * as XLSX from 'xlsx';
+
+interface CandidateExcelData {
+  seniority: string;
+  years: number;
+  availability: boolean;
+}
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  processCandidate(name: string, surname: string, fileBuffer: Buffer) {
+    const workbook = XLSX.read(fileBuffer, { type: 'buffer' });
+
+    const sheetName = workbook.SheetNames[0];
+    const sheet = workbook.Sheets[sheetName];
+    const data = XLSX.utils.sheet_to_json<CandidateExcelData>(sheet, {
+      header: ['seniority', 'years', 'availability'],
+      range: 0, // start at first row,
+    })[0];
+
+    return {
+      name,
+      surname,
+      seniority: data.seniority,
+      years: data.years,
+      availability: data.availability,
+    };
   }
 }
