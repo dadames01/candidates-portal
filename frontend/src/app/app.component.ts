f4a3cb099cf0fffb3cc694ed37a7ff58b80b3152
Daniel Adames <danieladames24@gmail.com>
2025-08-04 00:06:29 +0200

    feat: move table logic to standalone component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,15 +1,12 @@
 import { Component, signal, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
 import { CandidatesApiService } from './candidates-api.service';
-import { MatTableModule } from '@angular/material/table';
 import { CandidatesFormComponent } from "./candidates-form/candidates-form.component";
+import { CandidatesTableComponent } from "./candidates-table/candidates-table.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CandidatesFormComponent, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatTableModule, CandidatesFormComponent],
+  imports: [RouterOutlet, CandidatesFormComponent, CandidatesTableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -17,16 +14,14 @@ export class AppComponent {
   constructor(private apiService: CandidatesApiService) { }
   @ViewChild('candidatesForm') candidatesFormComponent!: CandidatesFormComponent;
 
-  displayedColumns = ['name', 'surname', 'seniority', 'years', 'availability']
-  dataSource = signal<any[]>(this.loadCandidatesData());
+  candidatesData = signal<any[]>(this.loadCandidatesData());
 
   handleCandidatesFormSubmit(formData: FormData, f: any) {
     this.apiService.postData(formData).subscribe({
       next: (res) => {
-        console.log('API success:', res);
         this.candidatesFormComponent.resetForm(f);
-        this.dataSource.set([...this.dataSource(), res])
-        this.saveCandidatesData(this.dataSource());
+        this.candidatesData.set([...this.candidatesData(), res])
+        this.saveCandidatesData(this.candidatesData());
       },
       error: (err) => {
         console.error('API error:', err);
